e1226bde6f111d0d33308aef629d1eeb54fdfb80
maxikgreat <maximvasylenko228322@gmail.com>
2020-11-16 22:35:17 +0100

    Fix

--- src/app/AppUtils.ts
+++ src/app/AppUtils.ts
@@ -3,9 +3,11 @@ import morgan from 'morgan';
 export const httpLogger = () => {
   return morgan
     .token('headers', (req) => JSON.stringify(req.headers))
+    // @ts-ignore
     .token('protocol', (req) => req.protocol)
     .token(
       'host',
+      // @ts-ignore
       (req) => req.get('host')!,
     )(
     ':headers:\n:protocol :host :method :url :status :res[content-length] - :response-time ms',
